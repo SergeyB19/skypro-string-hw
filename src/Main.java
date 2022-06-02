50d96b8cb2134b23f3773e2fe9a66df3702efe5d
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-06-02 09:40:04 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // №1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника - " + fullName);
+        // №2
+        fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
     }
 }
